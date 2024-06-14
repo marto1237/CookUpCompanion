a20a5c0789daad9cd8246979a56e355ef497d6ad
Martin Simeonov <i525452@fhict.nl>
2024-06-14 08:45:14 +0200

    Final fix

--- CookUp Companion/BusinessLogic/Algoritam/RecommendedRecipesAlgoritam.cs
+++ CookUp Companion/BusinessLogic/Algoritam/RecommendedRecipesAlgoritam.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace CookUp_Companion_BusinessLogic.Algoritam
 {
-    public class RecommendedRecipesAlgoritam /*: IRecommendedRecipesAlgoritam*/
+    public class RecommendedRecipesAlgoritam : IRecommendedRecipesAlgoritam
     {
         private readonly IRecipeDALManager recipeManager;
         private readonly IUserManager userManager;
@@ -62,15 +62,116 @@ namespace CookUp_Companion_BusinessLogic.Algoritam
             }
         }
 
-        private List<Recipe> GetUserLikedRecipes(int userId)
+        public List<Recipe> GetUserLikedRecipes(int userId)
         {
             // Assuming there's a way to get all liked recipes by a user's ID
             return recipeReviewsManager.GetLikedRecipesByUser(userId);
         }
 
+
         private List<Recipe> GetUserSavedRecipes(int userId)
         {
             return recipeManager.GetSavedRecipesByUser(userId);
+        }
+
+        public List<Recipe> RecommendTrendingRecipes(int pageNumber, int pageSize)
+        {
+            List<Recipe> allRecipes = recipeManager.GetAllRecipes(1, int.MaxValue);
+            DateTime referenceDate = DateTime.Now;
+
+            var trendingRecipes = allRecipes
+                .OrderByDescending(recipe => CalculateTrendScore(recipe, referenceDate))
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            return trendingRecipes;
+        }
+
+        public bool IsRecipeSimilar(Recipe one, Recipe two)
+        {
+            return AreRecipesSimilar(one, two);
+        }
+
+        public double CalculateTrendScore(Recipe recipe, DateTime referenceDate)
+        {
+            int recipeID = GetRecipeId(recipe);
+            DateTime creationDate = GetRecipeCreateDate(recipeID);
+            (int likes, int dislikes) = GetRecipeLikesAndDislikes(recipeID);
+            int saveCount = GetRecipeSaveCount(recipeID);
+
+            double daysSinceCreation = (referenceDate - creationDate).TotalDays;
+            double likeRatio = likes > 0 ? (double)likes / (likes + dislikes) : 0;
+            double trendScore = (likeRatio * saveCount) / daysSinceCreation;
+
+            return trendScore;
+        }
+
+        public List<Recipe> CombineRecipesForTrends(int pageNumber, int pageSize)
+        {
+            List<Recipe> allRecipes = recipeManager.GetAllRecipes(1, int.MaxValue);
+
+            var combinedRecipes = allRecipes
+                .OrderByDescending(recipe => CalculateTrendScore(recipe, DateTime.Now))
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            return combinedRecipes;
+        }
+
+        public DateTime GetRecipeCreateDate(int recipeId)
+        {
+            return recipeManager.GetRecipeCreateDate(recipeId);
+        }
+
+        public (int Likes, int Dislikes) GetRecipeLikesAndDislikes(int recipeId)
+        {
+            return recipeReviewsManager.GetLikesAndDislikes(recipeId);
+        }
+
+        public int GetRecipeSaveCount(int recipeId)
+        {
+            return recipeManager.GetSaveCount(recipeId);
+        }
+
+        public int GetRecipeId(Recipe recipe)
+        {
+            return recipeManager.GetRecipeID(recipe);
+        }
+
+        public int GetTotalRecommendedRecipes(User user)
+        {
+            try
+            {
+                List<Recipe> allRecipes = recipeManager.GetAllRecipes(1, int.MaxValue);
+                int userId = userManager.GetIdByUsername(user.Username);
+
+                List<Recipe> userLikedRecipes = GetUserLikedRecipes(userId);
+                List<Recipe> userSavedRecipes = GetUserSavedRecipes(userId);
+
+                HashSet<string> excludedRecipeNames = new HashSet<string>(userLikedRecipes.Select(r => r.RecipeName));
+                excludedRecipeNames.UnionWith(userSavedRecipes.Select(r => r.RecipeName));
+
+                List<Recipe> recipesToRecommend = allRecipes.Where(recipe => !excludedRecipeNames.Contains(recipe.RecipeName)).ToList();
+
+                var recommendedRecipes = new HashSet<Recipe>();
+
+                foreach (var recipe in recipesToRecommend)
+                {
+                    if (IsMatchingUserPreferences(recipe, userLikedRecipes))
+                    {
+                        recommendedRecipes.Add(recipe);
+                    }
+                }
+
+                return recommendedRecipes.Count;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error generating total recommended recipes: {ex.Message}");
+                return 0;
+            }
         }
 
         private bool AreRecipesSimilar(Recipe one, Recipe two)
